ac1838fc64d60e6669d510f33d5476257a6bd871
theoaks78 <theoaks78@gmail.com>
2026-09-17 06:35:01 +1000

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
    it only grants what Row Level Security allows (read-only).
    ============================================================ */
 const DINO = {
-  SUPABASE_URL: "https://gzaaqdfyugkbwghuveal.supabase.co/",
+  SUPABASE_URL: "https://gzaaqdfyugkbwghuveal.supabase.co",
   API_KEY: "sb_publishable_wpYQ66d1K9xm7T339ZS-Xg_lsLlZ9C4"
 };
